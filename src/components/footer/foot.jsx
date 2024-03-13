fe472a69959b412c90ee26710f0fc761c38a0f9b
510rrl <rrlopez@uci.edu>
2024-03-13 10:57:34 -0700

    added files and changed names

---
+++ src/components/footer/foot.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div>
+            <h2>Footer</h2>
+            <p>
+                Copyright TM RRL MMXXIV TM
+            </p>
+        </div>
+    );
+}
